451e87098f608d010e5af37b4f59656b5fb4ee9d
ndtsparka <71079621+ndtsparka@users.noreply.github.com>
2020-09-15 15:57:07 +0700

    Add files via upload

---
+++ Section 2_Var/var.js
@@ -0,0 +1,66 @@
+var a;
+let b;
+
+a = 5;
+b = 10;
+
+console.log(a);
+console.log(b);
+
+a = 50;
+b = 100;
+
+console.log(a);
+console.log(b);
+
+const pi = 3.14; 
+
+console.log(pi);
+
+let c;
+c = a + b;
+console.log(c);
+
+
+let d;
+d = a * b;
+console.log(d);
+
+let e;
+e = a / b
+console.log(e);
+
+var f;
+let q;
+f = 90;
+q = 70;
+i = f % q;
+console.log(i);
+
+let t 
+t = f**2;
+console.log(t);
+
+let u;
+u = Math.sqrt(f);
+console.log(u);
+
+let y;
+y = f**3;
+console.log(y);
+
+//let name = prompt("Nhập tên"); 
+//console.log("Xin chao " + name);
+// nhập tên
+
+ 
+//let age = prompt("Nhập tuổi");
+//console.log("Bạn " + age + " tuổi");
+// nhập tuổi
+
+// let yearOfBirth = prompt("Nhập năm sinh")
+// let age = 2020 - yearofbirth;
+// console.log("Bạn " + age + " tuổi");
+
+
+
